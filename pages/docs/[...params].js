70ca38c15ac0c9355c20bfa65bf7a2d468e7afa0
farukwebcode21 <farukahmadmk@yahoo.com>
2023-04-30 16:00:54 +0600

    Learn Link

--- pages/docs/[...params].js
+++ pages/docs/[...params].js
@@ -1,6 +1,10 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const Doc = () => {
+  const router = useRouter();
+  const { params } = router.query;
+  console.log(params);
   return (
     <div>
       <h1>Doc Home Page</h1>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <nav>
+        <Link href="/blog">Blog</Link>
+        <Link href="/products">Product</Link>
+      </nav>
     </div>
   );
 };
